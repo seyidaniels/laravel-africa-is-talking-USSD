561da2b934378fa0c3f69515be60b9b5a43331f8
MAC <mac@MACs-MacBook-Pro.local>
2019-08-14 11:11:10 +0100

    Initial commit

---
+++ app/Http/Controllers/USSDController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class USSDController extends Controller
+{
+    public function index (Request $request) {
+        $sessionId   = $request["sessionId"];
+        $serviceCode = $request["serviceCode"];
+        $phoneNumber = $request["phoneNumber"];
+        $text        = $request["text"];
+
+        header('Content-type: text/plain');
+        echo "CON we just getting started";
+
+    }
+
+
+    protected function getUser ($phoneNumber) {
+        
+    }
+}
