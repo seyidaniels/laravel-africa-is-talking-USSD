78a86ea30918e79f5e684f4151eedc0ba2603871
MAC <mac@MACs-MacBook-Pro.local>
2019-08-14 11:23:14 +0100

    changes again

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -7,18 +7,19 @@ use Illuminate\Http\Request;
 class USSDController extends Controller
 {
     public function index (Request $request) {
-        $sessionId   = $request["sessionId"];
-        $serviceCode = $request["serviceCode"];
-        $phoneNumber = $request["phoneNumber"];
-        $text        = $request["text"];
+        // $sessionId   = $request["sessionId"];
+        // $serviceCode = $request["serviceCode"];
+        // $phoneNumber = $request["phoneNumber"];
+        // $text        = $request["text"];
 
-        header('Content-type: text/plain');
-        echo "CON we just getting started";
+        // header('Content-type: text/plain');
+        // echo "CON we just getting started";
+        echo "Seyi Daniels";
 
     }
 
 
     protected function getUser ($phoneNumber) {
-        
+
     }
 }
