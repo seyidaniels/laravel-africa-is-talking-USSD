e7de975f919f12bba229a77735780f1b41d96c88
MAC <mac@MACs-MacBook-Pro.local>
2019-08-14 11:36:03 +0100

    changed to laravel

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -7,14 +7,13 @@ use Illuminate\Http\Request;
 class USSDController extends Controller
 {
     public function index (Request $request) {
-        // $sessionId   = $request["sessionId"];
-        // $serviceCode = $request["serviceCode"];
-        // $phoneNumber = $request["phoneNumber"];
-        // $text        = $request["text"];
+        $sessionId   = $request["sessionId"];
+        $serviceCode = $request["serviceCode"];
+        $phoneNumber = $request["phoneNumber"];
+        $text        = $request["text"];
 
-        // header('Content-type: text/plain');
-        // echo "CON we just getting started";
-        echo "Seyi Daniels";
+        header('Content-type: text/plain');
+        echo "CON we just getting started";
 
     }
 
